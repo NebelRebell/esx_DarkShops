88cd3cf8586f531cafed46d24072196e581fdd96
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2018-08-28 15:26:43 +0200

    Update de.lua

--- locales/de.lua
+++ locales/de.lua
@@ -2,7 +2,7 @@ Locales['de'] = {
 
 	['shop'] 		= 'Geschäft',
 	['shops'] 		= 'Geschäfte',
-	['press_menu'] 	= 'Benutze ~INPUT_CONTEXT~ um auf das Geschäft zuzugreifen.',
+	['press_menu'] 		= 'Benutze ~INPUT_CONTEXT~ um auf das Geschäft zuzugreifen.',
 	['bought'] 		= 'Du kaufst ~b~1x ',
-	['not_enough'] 	= 'Du ~r~hast nicht~s~ genug Geld!'
+	['not_enough'] 		= 'Du ~r~hast nicht~s~ genug Geld!'
 }
